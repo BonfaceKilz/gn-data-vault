d949123cc8da055b806b7e7c0cfc476aa17eab5c
Alexander_Kabui <alexanderkabua@gmail.com>
2023-07-20 02:43:59 +0300

    init parser line data and extract args

--- genofile-parser.scm
+++ genofile-parser.scm
@@ -48,3 +48,43 @@
             (loop (cons (parse-genofile-function line) lines) (read-line input-port)))))))
 
 
+
+(define (parse-genotype-marker line geno-obj parlist)
+  (define marker-row (map string-trim (string-split line "\t")))
+
+
+  (define geno-table
+    `((,(hash-ref geno-obj "mat") . -1)
+      (,(hash-ref geno-obj "pat") . 1)
+      (,(hash-ref geno-obj "het") . 0)
+      (,(hash-ref geno-obj "unk") . "U")))
+  (define start-pos (if (hash-ref geno-obj "Mbmap") 4 3))
+  (when (> (length parlist) 0)
+    (set! start-pos (+ start-pos 2)))
+
+  (define alleles (drop marker-row start-pos))
+  (define genotype
+    (map (lambda (allele)
+           (if (hash-has-key? geno-table allele)
+               (hash-ref geno-table allele)
+               "U"))
+         alleles))
+  (when (> (length parlist) 0)
+    (set! genotype (cons -1 (cons 1 genotype))))
+  (define cm-val
+    (with-handlers ((condition? (lambda (_) #f)))
+      (let ((cm-column (hash-ref geno-obj "cm_column"))
+            (mb-column (hash-ref geno-obj "mb_column")))
+        (if (hash-ref geno-obj "Mbmap")
+            (string->number mb-column)
+            (string->number cm-column)))))
+  (list
+    (cons "chr" (list-ref marker-row 0))
+    (cons "name" (list-ref marker-row 1))
+    (cons "cM" cm-val)
+    (cons "Mb"
+          (if (hash-ref geno-obj "Mbmap")
+              (string->number (hash-ref geno-obj "mb_column"))
+              #f))
+    (cons "genotype" genotype)))
+
