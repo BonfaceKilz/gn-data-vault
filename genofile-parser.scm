e90e8b36e12d402bd72d4a4d4127832469e6b5fa
Alexander_Kabui <alexanderkabua@gmail.com>
2023-07-20 02:41:49 +0300

    read file line by line

--- genofile-parser.scm
+++ genofile-parser.scm
@@ -2,6 +2,8 @@
 
 (use-modules (ice-9 rdelim))
 
+
+
 (define (string-starts-with? str prefix)
   (define prefix-length (string-length prefix)) 
   (and (>= (string-length  str) prefix-length)
@@ -12,3 +14,19 @@
 (define (hashtable->alist ht)
   (let-values (((ks vs) (hashtable-entries ht)))
     (vector->list (vector-map cons ks vs))))
+
+
+
+(define (read-file-line-by-line filename)
+  (call-with-input-file filename
+    (lambda (input-port)
+      (let loop ((lines '())
+                 (line (read-line input-port)))
+        (if (eof-object? line)
+            (reverse lines) ; Return the lines in reverse order
+            (loop (cons line lines) (read-line input-port)))))))
+
+
+
+
+
