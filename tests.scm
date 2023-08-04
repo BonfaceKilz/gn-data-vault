834ed12a739e7e1e3dfbb1f939b74dc0d3d17a7d
Munyoki Kilyungi <me@bonfacemunyoki.com>
2023-08-04 17:05:57 +0300

    Remove dead space
    * tests.scm: Remove dead space
    
    Signed-off-by: Munyoki Kilyungi <me@bonfacemunyoki.com>

--- tests.scm
+++ tests.scm
@@ -1,11 +1,6 @@
-
-
-
-
 (use-modules (srfi srfi-64)
              (ice-9 hash-table)
-	          (genofile-parser ))
-
+             (genofile-parser ))
 
 (test-begin "genofile parser")
 
@@ -21,31 +16,26 @@
 
 
 (test-equal "test parser for labels" (list "@this is a test" #f "@third line")
-  (genofiles-line-parsers (list "@this is a test" "second line" "@third line") 
-  parse-genofile-labels) )
+            (genofiles-line-parsers (list "@this is a test" "second line" "@third line")
+                                    parse-genofile-labels) )
 
-
-(test-equal "test parser for headers" (list  "#header1:name" #f #f)
-  (genofiles-line-parsers (list "#header1:name" "header2" "@header3")
-  parse-genofile-headers
-  )
-)
-
-
+(test-equal "test parser for headers" (list "#header1:name" #f #f)
+	    (genofiles-line-parsers (list "#header1:name" "header2" "@header3")
+				    parse-genofile-headers))
 
 (test-equal "Test valid label"
-              (parse-label "  name:alex")
-              '("group" . "alex"))
+  (parse-label "  name:alex")
+  '("group" . "alex"))
 
 (test-equal "Test invalid label"
-            (parse-label "  invalid_label  :  SomeValue  ")
-            #f)
+  (parse-label "  invalid_label  :  SomeValue  ")
+  #f)
 (test-equal "Test other labels"
-            (parse-label " type:foo")
-            '("type" . "foo"))
-
+  (parse-label " type:foo")
+  '("type" . "foo"))
 
 (define my-hash (make-hash-table))
+
 (hash-set! my-hash "mat" 12)
 (hash-set! my-hash "A"  1)
 (hash-set! my-hash "pat" 3)
@@ -54,19 +44,18 @@
 (hash-set! my-hash "C"  12)
 (hash-set! my-hash  "unk" "test")
 (hash-set! my-hash  "N"  "q")
- (define  expected-results
-   `(("chr" . "chr1")
-     ("name" . "Marker1")
-     ("cM" . 12.1)
-     ("Mb" . #f)
-     ("genotype" "U" "U" "U")))
+
+(define  expected-results
+  `(("chr" . "chr1")
+    ("name" . "Marker1")
+    ("cM" . 12.1)
+    ("Mb" . #f)
+    ("genotype" "U" "U" "U")))
 
 (define results (parse-genotype-marker "chr1\tMarker1\tA\tA\tC\tT"
-                                       my-hash
-                                       '()))
+				       my-hash
+				       '()))
 (test-equal "Test parsing genofile markers" expected-results results)
-
-
 
 (test-equal "test file reading" (parse-genotype-file "name.txt") '())
 
